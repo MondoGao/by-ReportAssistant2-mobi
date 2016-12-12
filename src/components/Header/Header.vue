71f18d8eb49977ede3b045a66e9a8e85bae06a7b
风之雪 <tiancaigaohua@163.com>
2016-12-12 23:21:40 +0800

    + add search by catalog  + add transition for catalog  * change icon's font * update readme.md

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,6 +1,23 @@
 <template>
   <header id="search">
-    <a href="#" class="back-home"></a>
+    <div :class="{ catalog: true, 'show-catalog': isShowCatalog }"
+      @click="isShowCatalog = !isShowCatalog">
+      <transition name="fade">
+        <div class="black-layout" v-show="isShowCatalog">
+          <transition name="slide">
+            <div v-show="isShowCatalog">
+              <transition-group tag="ul" name="list">
+                <li v-for="(value, index) of catalog"
+                    :key="index"
+                    v-show="isShowCatalog"
+                    :style="{ transition-delay: 0.3 * index + 's' }"
+                    @click="clickCatalog">{{ value }}</li>
+              </transition-group>
+            </div>
+          </transition>
+        </div>
+      </transition>分类<!--
+      --></div>
     <div class="search-input-field">
       <input type="text" id="search-input" placeholder="搜索学院或文档"
       @focus="toggleSubmit"
@@ -10,7 +27,7 @@
     </div>
     <div
        @click="toggleSubmit(), search()"
-       :class="{'cancel-btn':true, show: showSubmit}">搜索</div>
+       :class="{'search-btn':true, show: showSubmit}">搜索</div>
   </header>
 </template>
 
@@ -20,7 +37,27 @@
     data () {
       return {
         showSubmit: false,
-        searchStr: ''
+        searchStr: '',
+        isShowCatalog: false,
+        catalog: [
+          '公选课',
+          '实验报告',
+          '自动化学院',
+          '电子信息与通信学院',
+          '软件学院',
+          '光学与电子信息学院',
+          '计算机科学与技术学院',
+          '电气与电子工程学院',
+          '材料科学与工程学院',
+          '生命科学与技术学院',
+          '化学与化工学院',
+          '物理学院',
+          '数学与统计学院',
+          '新闻与信息传播学院',
+          '建筑与城市规划学院',
+          '土木科学与工程学院',
+          '能源与动力工程学院'
+        ]
       }
     },
     computed: {
@@ -34,6 +71,10 @@
           path: '/search'
         })
         this.$root._bus.$emit('search', this.searchStr)
+      },
+      clickCatalog (e) {
+        this.searchStr = e.srcElement.innerText
+        this.search()
       }
     },
     mounted: function () {
