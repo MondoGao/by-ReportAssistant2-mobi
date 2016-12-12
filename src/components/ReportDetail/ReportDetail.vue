3f10347a7ebf8e7ace2accce0ba491a018487316
风之雪 <tiancaigaohua@163.com>
2016-12-12 20:36:32 +0800

    + finish report's preview  * fix bug with flexible

--- src/components/ReportDetail/ReportDetail.vue
+++ src/components/ReportDetail/ReportDetail.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="detail-container">
+  <div :class="{ 'detail-container': true, scrollable: isSmallScreen }">
     <section class="loading-icon"
-    v-if="!meta">
+    v-show="!meta || isLoading">
       <img src="~assets/loading.gif">
     </section>
-    <template v-else>
+    <template v-if="meta">
       <ReportMeta
         :meta="meta"></ReportMeta>
       <section :class="{ 'report-preview-container': true, fullscreen: isFullScreen }">
@@ -12,13 +12,14 @@
           <div id="btn-type-info" :class="{ 'report-type-info': true, scroll: isScrolling }">{{ meta.type }}文档预览</div>
           <div id="btn-fullscreen" :class="{ 'report-type-info': true, scroll: isScrolling }"
             @click="isFullScreen = !isFullScreen"></div>
-          <iframe :src="meta.preview" id="ifr-container"></iframe>
+          <iframe :src="meta.preview" id="ifr-container"
+            @load="previewLoad"></iframe>
         </template>
         <img src="~assets/report-preview-miss.png" v-else>
       </section>
       <section class="report-download">
         <a href="javascript:;" class="download-btn"
-           @click="isShowDownload = true"
+           @click="toggleDownload"
            v-if="!isShowDownload">立即下载</a>
         <div class="report-download-container" v-else>
           <p>请你在“iKnow华科”公众号回复该编号以获取下载链接，文档仅供参考。</p>
@@ -39,7 +40,9 @@
         meta: null,
         isShowDownload: false,
         isFullScreen: false,
-        isScrolling: false
+        isScrolling: false,
+        isLoading: true,
+        isSmallScreen: false
       }
     },
     components: {
@@ -49,14 +52,53 @@
       getData (id) {
         this.$http.get('/document_detail/' + id + '/mobi', {emulateJSON: true})
           .then(function (response) {
-            let data = response.data
-            this.meta = data.result
-            console.log(this.meta)
-            document.title = '我在"报告菌"上找到了' + data.result.document_name + ',这下不用担心了'
+            /* Todo: dev */
+            response.data.result.preview = response.data.result.preview.replace('report.hustonline.net', 'localhost:8080')
+
+            this.meta = response.data.result
+            document.title = '我在"报告菌"上找到了' + this.meta.document_name + ',这下不用担心了'
+            if (!this.meta.preview) {
+              this.isLoading = false
+            }
           })
+      },
+      isScroll: (function () {
+        let _scrollTop = 0
+        let deltaTop = 0
+        return function (e) {
+          deltaTop = e.target.scrollTop - _scrollTop
+          _scrollTop = e.target.scrollTop
+          if (deltaTop > 10) {
+            this.isScrolling = true
+          } else if (deltaTop < 0) {
+            this.isScrolling = false
+          }
+        }
+      })(),
+      previewLoad (e) {
+        let ifr = e.path[0]
+        let ifrDoc = ifr.contentDocument || ifr.contentWindow.document
+        let ifrHead = ifrDoc.getElementsByTagName('head')[0]
+        let ifrStyle = document.createElement('style')
+        let container = ifrDoc.getElementById('page-container')
+        let pf = ifrDoc.getElementById('pf1')
+        let scale = container.offsetWidth / pf.offsetWidth
+        /* eslint-disable quotes */
+        let scaleTxt = "div[id^='pf']{width:" + container.offsetWidth + "px;height:" + pf.offsetHeight * scale + "px}.pc{-webkit-transform: scale(" + scale + ");transform:scale(" + scale + ");}"
+
+        let touchTxt = "#page-container{-webkit-overflow-scrolling: touch;}"
+        ifrStyle.setAttribute('type', 'text/css')
+        ifrHead.appendChild(ifrStyle)
+        ifrStyle.innerHTML = scaleTxt + ' ' + touchTxt
+
+        container.addEventListener('scroll', this.isScroll)
+
+        this.isLoading = false
+      },
+      toggleDownload () {
+        this.isShowDownload = true
       }
     },
-    watch: {},
     created () {
       this.getData(this.$route.params.id)
     }
